6fee21bf2b2a4d9c8df4292b4318895869bfe6b1
Cody Pafford <codypafford6@gmail.com>
2025-05-30 17:42:43 -0400

    update cors

--- server/index.js
+++ server/index.js
@@ -21,9 +21,19 @@ dotenv.config({
 
 
 // Enable CORS for your frontend origin
+const allowedOrigins = ['http://localhost:5173', 'https://networth-frontend-8gem.onrender.com'];
+
 app.use(cors({
-  origin: 'http://localhost:5173',  // Allow this origin only
-  credentials: true,                // Allow cookies and auth headers
+  origin: function(origin, callback) {
+    // allow requests with no origin (like mobile apps or curl)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true,
 }));
 
 app.use(express.json()); // for parsing application/json
