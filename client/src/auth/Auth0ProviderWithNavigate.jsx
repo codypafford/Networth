94ba15afdbd9401efdd4788bec9744c9a37c4506
Cody Pafford <codypafford6@gmail.com>
2025-05-31 08:36:37 -0400

    try to fix refresh

--- client/src/auth/Auth0ProviderWithNavigate.jsx
+++ client/src/auth/Auth0ProviderWithNavigate.jsx
@@ -1,32 +1,33 @@
-import { useNavigate } from 'react-router-dom';
-import { Auth0Provider } from '@auth0/auth0-react';
-import PropTypes from 'prop-types';
+import { useNavigate } from 'react-router-dom'
+import { Auth0Provider } from '@auth0/auth0-react'
+import PropTypes from 'prop-types'
 
 const Auth0ProviderWithNavigate = ({ children }) => {
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
-  const domain = import.meta.env.VITE_AUTH0_DOMAIN;
-  const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
-  const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
+  const domain = import.meta.env.VITE_AUTH0_DOMAIN
+  const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID
+  const audience = import.meta.env.VITE_AUTH0_AUDIENCE
 
   return (
     <Auth0Provider
       domain={domain}
       clientId={clientId}
       authorizationParams={{
-      redirect_uri: window.location.origin,
-      audience: audience,
-      // scope: "read:current_user update:current_user_metadata"
-  }}
+        redirect_uri: window.location.origin,
+        audience: audience
+        // scope: "read:current_user update:current_user_metadata"
+      }}
       onRedirectCallback={() => navigate('/')}
+      cacheLocation='localstorage'
     >
       {children}
     </Auth0Provider>
-  );
-};
+  )
+}
 
 Auth0ProviderWithNavigate.propTypes = {
   children: PropTypes.node.isRequired
 }
 
-export default Auth0ProviderWithNavigate;
+export default Auth0ProviderWithNavigate
